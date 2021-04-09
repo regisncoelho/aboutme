bff186ff7f2f3f63f7a96212138247ef55842f77
Régis Coelho <81445653+regisncoelho@users.noreply.github.com>
2021-04-08 22:16:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+var aboutMe = {
+    nome: "Régis Coelho",
+    sobre: ""
+}
+
+var aula1 = {
+    nome: "Conversor de Moedas",
+    sobre: "",
+    urlCodePen: "https://codepen.io/regisncoelho/pen/WNRNaza",
+    urlGithub: ""
+}
+
+var aula2 = {
+    nome: "Calculadora",
+    sobre: "",
+    urlCodePen: "https://codepen.io/regisncoelho/pen/yLgNOwG",
+    urlGithub: ""
+}
+
+var aula3 = {
+    nome: "",
+    sobre: "",
+    urlCodePen: "https://codepen.io/regisncoelho/pen/yLgYXBR",
+    urlGithub: ""
+}
+
+var aula31 = {
+    nome: "Jogo de Tabuada",
+    sobre: "",
+    urlCodePen: "https://codepen.io/regisncoelho/pen/xxgwLBj",
+    urlGithub: "https://regisncoelho.github.io/jogotabuada/"
+}
+
+var aula4 = {
+    nome: "Aluraflix (projeto extra)",
+    sobre: "",
+    urlCodePen: "https://codepen.io/regisncoelho/pen/qBRbKgv",
+    urlGithub: ""
+}
+
+var aula5 = {
+    nome: "Aluraflix",
+    sobre: "",
+    urlCodePen: "https://codepen.io/regisncoelho/pen/abpNEbz",
+    urlGithub: ""
+}
+
+var aula6 = {
+    nome: "Tabela de Classificação",
+    sobre: "",
+    urlCodePen: "https://codepen.io/regisncoelho/pen/BapLxbx",
+    urlGithub: ""
+}
+
+var aula7a9 = {
+    nome: "Supertrunfo (projeto extra)",
+    sobre: "",
+    urlCodePen: "https://codepen.io/regisncoelho/pen/poRNZwE",
+    urlGithub: "https://regisncoelho.github.io/batalhabey/"
+}
+
+var aula10 = {
+    nome: "Certificard",
+    sobre: "",
+    urlCodePen: "https://codepen.io/regisncoelho/pen/JjEJExE",
+    urlGithub: "https://regisncoelho.github.io/certificado-imersaodev/"
+}
+
